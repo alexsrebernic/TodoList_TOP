3ac2fd23833521f10ce72c36d588932cb4d9f9c1
Alex Srebernic <alexsrebernic@gmail.com>
2021-10-27 16:49:25 -0300

    start js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const HOME_BUTTON = document.getElementById("home")
+const ADD_PROJECT_BUTTON = document.getElementById("add_project")
+const CALENDAR_BUTTON = document.getElementById("calendar")
+
+HOME_BUTTON.onclick = () => console.log("asd")
