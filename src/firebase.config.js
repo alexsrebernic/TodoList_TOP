4f07a11d2cb38038d0c7dd81326856df3540752f
Alex Srebernic <alexsrebernic@gmail.com>
2021-12-02 09:38:46 -0300

    ignore firebaseconfig.js

---
+++ src/firebase.config.js
@@ -0,0 +1,22 @@
+export const firebaseConfig = {
+
+    apiKey: "AIzaSyB_9bN8wsncvuVCIEnzYIFhg-mwWFu9T_s",
+ 
+   authDomain: "to-do-app-780b2.firebaseapp.com",
+ 
+   databaseURL: "https://to-do-app-780b2-default-rtdb.firebaseio.com",
+ 
+   projectId: "to-do-app-780b2",
+ 
+   storageBucket: "to-do-app-780b2.appspot.com",
+ 
+   messagingSenderId: "411421814027",
+ 
+   appId: "1:411421814027:web:de143fb0d153c8fa8dc60e",
+ 
+   measurementId: "G-WEEDGLSV32"
+  };
+ export const actionCodeSettings = {
+    url: 'https://alexsrebernic.github.io/TodoList_TOP/',
+    handleCodeInApp: true,
+  };
